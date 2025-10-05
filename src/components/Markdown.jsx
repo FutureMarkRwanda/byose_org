0f9fc5e10455579f02c7c0eec524849c4f0ecd7a
Hirwa Rukundo Hope <120049248+hrh2@users.noreply.github.com>
2025-10-05 13:00:57 +0200

    fixing md

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,13 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
 import "highlight.js/styles/github.css"; // Light mode version
 import "katex/dist/katex.min.css"; // Import KaTeX CSS
-import remarkGfm from "remark-gfm";
-import rehypeSanitize from "rehype-sanitize";
+
 
 // eslint-disable-next-line react/prop-types
 export default function Markdown({content}) {
@@ -15,12 +11,6 @@ export default function Markdown({content}) {
         <div className="markdown-body md:text-2xl text-xl xl:w-[70%] mx-auto">
             <ReactMarkdown
                 class="flex flex-col"
-                remarkPlugins={[remarkGfm, remarkMath]}
-                rehypePlugins={[
-                    [rehypeSanitize], // ✅ Safe fallback for old iOS
-                    rehypeHighlight,
-                    rehypeKatex,
-                ]}
             >
                 {content}
             </ReactMarkdown>
