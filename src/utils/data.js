6eb2eab79ef1faaed372e438dd0f16dbfb7f363f
Hirwa Rukundo Hope <120049248+hrh2@users.noreply.github.com>
2026-09-11 12:28:24 +0200

    Added: link data for leonce

--- src/utils/data.js
+++ src/utils/data.js
@@ -317,7 +317,8 @@ Innocent’s leadership in technology is characterized by his commitment to inno
         role:"Co-Founder, B-Skills | Chief Innovation Officer",
         images:[
             "https://res.cloudinary.com/de5pz88gm/image/upload/v1789119662/Projects/porto/WhatsApp_Image_2026-09-11_at_10.03.07_AM_iliywz.jpg"
-        ]
+        ],
+        link:"leonce-gisagara-hirwa"
     },
     // {
     //     id: 115,
